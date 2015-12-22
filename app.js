906036094e24d5a6e926c05afd465e9110c631fd
hanqilin <hqlkylin@gmail.com>
2015-12-22 21:24:52 +0800

    注册

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 
 
 var express = require('express');
+var path=require("path");
 var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.use(express.static(path.join(__dirname,"app","public")));
+//app.get('/', function (req, res) {
+//    res.send('Hello World')
+//})
 
 app.listen(3000);
